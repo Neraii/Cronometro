747d85add6d8cf48dcc627cea548ac548ea3ec5c
Cairo Henrique <cairothomaz@gmail.com>
2023-03-08 21:02:34 -0300

    Deixando responsivo

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,59 +1,72 @@
 class Cronometro {
-    constructor(caixa,numero) {
+    constructor(caixa, numero) {
         this.numero = numero;
         this.start = caixa.querySelector("#Start");
         this.stop = caixa.querySelector("#Stop");
         this.reset = caixa.querySelector("#Reset");
         this.timer;
     }
-    
-    comeca(){
-        console.log(this.start,this.stop,this.reset,this.numero.innerHTML);
-    }
-
-    comecar(){
+    comecar() {
         this.start.disabled = true;
         this.start.style.borderColor = "gray";
-       let numeroComeco = new Date;
+        let numeroComeco = new Date;
         this.passandoTempo(numeroComeco)
     }
-    passandoTempo(nComeco){
+    passandoTempo(nComeco) {
         let numeroFinal = new Date;
-        this.timer = setTimeout(() => this.passandoTempo(nComeco),100);
+        this.timer = setTimeout(() => this.passandoTempo(nComeco), 99);
         let subtracao = numeroFinal - nComeco;
         this.numero.innerHTML = this.#calculo(subtracao);
     }
 
-    parar(){
+    parar() {
         this.start.disabled = false;
         this.start.style.borderColor = "black";
         this.start.style.color = "black";
         clearInterval(this.timer);
     }
 
-    resetar(){
+    resetar() {
         this.parar();
-        this.numero.innerHTML = "00:00";
+        this.numero.innerHTML = "00:00:00:000";
     }
 
-    #calculo(milisegundos){
-        let segundo = Math.floor(milisegundos / 1000);
-        milisegundos %= 1000;
-        return `${segundo}:${milisegundos}`
+    #calculo(milisegundos) {
+        //milisegundos,segundos,minutos,horas
+
+
+        let tempo = [milisegundos, 0, 0, 0];
+        let divisores = [1000, 60, 60];
+        let texto = "";
+        for (let i = 0; i < 3; i++){
+            tempo[i + 1] = Math.round(tempo[i] / divisores[i]);
+            tempo[i] %= divisores[i]
+            texto =  this.#adicionarZero(tempo[i],i)+ ":"+ texto;
+        }
+        texto =  this.#adicionarZero(tempo[3],5)+ ":"+ texto;
+        //para as horas
+           texto = texto.slice(0,-1);
+        
+        return texto
     }
-    
+
+    #adicionarZero(numero,i) {
+        let quantidadeZero = numero.toString().length;
+        return ("0").repeat(( i > 0 ? 2 : 3) - quantidadeZero) + "" + numero;
+    }
+
 }
 
 
 const caixa = document.querySelector(".box");
 const numero = document.querySelector(".numero");
 
-let cronometro = new Cronometro(caixa,numero);
+let cronometro = new Cronometro(caixa, numero);
 
-const start= caixa.querySelector("#Start");
+const start = caixa.querySelector("#Start");
 const stopp = caixa.querySelector("#Stop");
 const reset = caixa.querySelector("#Reset");
 
-start.addEventListener("click",() => {cronometro.comecar()});
-stopp.addEventListener("click", () => { cronometro.parar()});
-reset.addEventListener("click", () => { cronometro.resetar()});   
+start.addEventListener("click", () => { cronometro.comecar() });
+stopp.addEventListener("click", () => { cronometro.parar() });
+reset.addEventListener("click", () => { cronometro.resetar() });   
